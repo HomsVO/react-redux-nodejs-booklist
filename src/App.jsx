e9655c3bdb7eb648c41f304253465beb18e4201c
HomsVO <max.vma@yandex.ru>
2020-03-17 11:58:52 +0300

    init prj

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap-css-only/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
+
+function App() {
+  return (
+      <div className="container">
+      </div>
+  );
+}
+
+export default App;
